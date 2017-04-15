3a480293f89ed52ecb81010daa041c6f9e62efe6
larsmsp <lars@atbrox.com>
2017-04-15 12:06:18 +0200

    Added gevent worker to gunicorn

---
+++ workshop/model/DocumentIndex.py
@@ -0,0 +1,34 @@
+from elasticsearch.helpers import bulk
+from elasticsearch_dsl import Index, analyzer
+from elasticsearch_dsl.connections import connections
+
+import Document
+
+
+def create(name, analyzer_name='norwegian'):
+    index = Index(name)
+    index.settings(
+        number_of_shards=2,
+        number_of_replicas=1
+    )
+    index.doc_type(Document)
+    index.analyzer(analyzer(analyzer_name))
+    index.delete(ignore=404)
+    index.create()
+    return exists(name)
+
+
+def exists(name):
+    return Index(name).exists()
+
+
+def index(name, documents):
+    conn = connections.get_connection()
+    actions = ({
+                   '_op_type': 'index',
+                   '_index': name,
+                   '_type': 'document',
+                   '_source': d
+               } for d in documents)
+    return bulk(conn, actions)[0]
+
