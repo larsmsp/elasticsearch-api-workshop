4a444b29ff38de53e8863d9c16c38eef0defb254
larsmsp <lars@atbrox.com>
2017-04-15 11:45:25 +0200

    Solution for exercise three

--- workshop/app.py
+++ workshop/app.py
@@ -1,1 +1,96 @@
+# coding=utf-8
+import logging
+import os
+import urllib2
 
+from elasticsearch_dsl.connections import connections
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from werkzeug.exceptions import BadRequest
+
+from model import DocumentIndex
+from model.Document import create_document
+from model import Document
+
+logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.getLogger().setLevel(logging.INFO)
+
+
+ES_HOSTS_ENV = 'ELASTICSEARCH_HOSTS'
+es_hosts = ['10.0.0.10'] if ES_HOSTS_ENV not in os.environ else str(os.environ[ES_HOSTS_ENV]).split(',')
+
+
+def verify_elasticsearch_connection():
+    logging.info("Verifying connection to Elasticsearch host(s)")
+    for h in es_hosts:
+        logging.info("Checking %s..." % h)
+        try:
+            urllib2.urlopen('http://%s' % h, timeout=2)
+        except urllib2.URLError:
+            raise RuntimeError('Unable to to connect to Elasticsearch host %s' % h)
+    logging.info("All good!")
+    return True
+
+
+verify_elasticsearch_connection()
+
+connections.create_connection(hosts=es_hosts)
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def root():
+    return render_template('root.html',
+                           elasticsearch_status='Connected' if verify_elasticsearch_connection() else 'Not connected')
+
+
+@app.route('/create/<string:index_name>', methods=['PUT'])
+def create(index_name):
+    if DocumentIndex.exists(index_name):
+        return 'An index with the name %s already exists.' % index_name, 409
+    if DocumentIndex.create(index_name):
+        return 'Index created', 200
+    else:
+        return 'Unable to create index', 500
+
+
+@app.route('/index/<string:index_name', methods=['POST'])
+def index_documents(index_name):
+    if request.is_json:
+        try:
+            json = request.get_json()
+            if DocumentIndex.exists(index_name):
+                indexed_documents = DocumentIndex.index(index_name,
+                                                        [create_document(index_name,
+                                                                         d['title'],
+                                                                         d['contents'],
+                                                                         d['url']) for d in json])
+                return jsonify({'indexed_documents': indexed_documents}), 200
+            else:
+                return 'Index "%s" does not exist.' % index_name, 408
+        except BadRequest:
+            return 'An exception was thrown parsing your JSON. Maybe you could try linting it?', 408
+    else:
+        return 'Request is not JSON. Ensure Content-Type is application/json.', 408
+
+
+@app.route('/update/<string:index_name>/<uuid:document_id>', methods=['PUT'])
+def update_document(index_name, document_id):
+    if request.is_json:
+        try:
+            json = request.get_json()
+            if Document.exists(index_name, document_id):
+                document = Document.create_document(index_name, document_id, json['title'], json['contents'], json['url'])
+                document.save()
+                return '', 200
+            else:
+                return 'Document "%s" does not exist in index "%s".' % (document_id, index_name), 404
+        except BadRequest:
+            return 'An exception was thrown parsing your JSON. Maybe you could try linting it?', 408
+    else:
+        return 'Request is not JSON. Ensure Content-Type is application/json.', 408
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ workshop/model/Document.py
@@ -0,0 +1,34 @@
+import uuid
+
+from datetime import datetime
+from elasticsearch_dsl import DocType, String, Date
+from elasticsearch import NotFoundError
+
+
+def create_document(index, id, title, contents, url):
+    document = Document(id, index)
+    document.title = title
+    document.contents = contents
+    document.url = url
+    return document
+
+
+def exists(document_id, index):
+    try:
+        Document.get(id=document_id, index=index)
+        return True
+    except NotFoundError:
+        return False
+
+
+class Document(DocType):
+
+    title = String()
+    contents = String()
+    url = String()
+    created_at = Date()
+
+    def __init__(self,_document_id,  _index, **kwargs):
+        super(Document, self).__init__(meta={'id': _document_id, 'index': _index}, **kwargs)
+        self.created_at = datetime.now()
+
