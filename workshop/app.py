c372b3b1551db0f3293a9cf17f31f4a32b67a930
larsmsp <lars@atbrox.com>
2017-04-15 11:57:22 +0200

    Slight change in documentation

--- workshop/app.py
+++ workshop/app.py
@@ -11,17 +11,22 @@ app = Flask(__name__)
 CORS(app)
 
 
-# Skal returnere "Hello, world!" tilbake til de som besøker siden.
 @app.route('/', methods=['GET'])
 def hello_world():
+    """
+    :return: "Hello, world!" til de som besøker siden. 
+    """
     pass
 
 
-# Skal returnere resultatet av enkle heltallsoperasjoner.
-# Operatorer og operand er angitt i query-parametrene "operator", "operand1" og "operand2", f.eks /greet?operator=plus&operand1=2&operand2=2
-# Endepunktet skal støtte "plus", "minus", "mult" og "div".
 @app.route('/calculator', methods=['GET'])
 def calculator():
+    """
+    Operatorer og operand er angitt i query-parametrene "operator", "operand1" og "operand2", 
+    f.eks /greet?operator=plus&operand1=2&operand2=2
+    Endepunktet skal støtte "plus", "minus", "mult" og "div".
+    :return: Resultatet av enkle heltallsoperasjoner.
+    """
     pass
 
 
