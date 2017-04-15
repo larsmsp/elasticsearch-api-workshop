5e544cf3369fbc34b5f7737289a093740dc8f671
larsmsp <lars@atbrox.com>
2017-04-15 14:15:57 +0200

    Fixed imports

--- workshop/app.py
+++ workshop/app.py
@@ -9,9 +9,9 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from werkzeug.exceptions import BadRequest
 
-from model import Document, Results
+from model import Document
 from model.Document import create_document
-from workshop.helper import DocumentIndex
+from helper import DocumentIndex, Results
 
 logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logging.getLogger().setLevel(logging.INFO)
