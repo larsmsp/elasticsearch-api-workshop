dc396527ce85c8e5c8746f793b4184b9b174a7c5
larsmsp <lars@atbrox.com>
2017-04-26 22:06:33 +0200

    Hopefully fixed relative imports
    (cherry picked from commit deb9b16)

--- workshop/helper/DocumentIndex.py
+++ workshop/helper/DocumentIndex.py
@@ -1,15 +1,21 @@
+import sys
+
+from elasticsearch_dsl import Index, analyzer, Search
+from os.path import join, abspath, dirname
 from elasticsearch_dsl import Index, analyzer
 
-import workshop.model.Document
+sys.path.append(join(dirname(abspath(__file__)), '../..'))
+
+from workshop.model.Document import Document
 
 
 def create(name, analyzer_name='norwegian'):
     index = Index(name)
     index.settings(
         number_of_shards=2,
-        number_of_replicas=1
+        number_of_replicas=0
     )
-    index.doc_type(workshop.model.Document)
+    index.doc_type(Document)
     index.analyzer(analyzer(analyzer_name))
     index.delete(ignore=404)
     index.create()
